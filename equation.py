affc69b0fba2ad0e144469bfd3da5bf84e27e6ea
Gerald Nash <g.nash.dev@gmail.com>
2017-03-02 21:08:14 -0500

    Add Latex Cheatsheet

--- equation.py
+++ equation.py
@@ -1,20 +1,23 @@
-import matplotlib.pyplot as plt                                                 
+import matplotlib.pyplot as plt
 import sympy
 
-limxtoinf = sympy.Symbol('\lim_{x\\to\infty}')
+#limxtoinf = sympy.Symbol('y = -x^2+1\n \\frac{dy}{dx} = -2x')
 
 # THE LATEX EXPRESSION TO BE PRINTED
-expr = sympy.latex(limxtoinf)
+#expr = sympy.latex(limxtoinf)
 # OR
-#expr = '\lim_{x\\to\infty}'
+y = r'y = x^3-3x^2 + 2x'
+dy = r'\frac{dy}{dx} = 3x^2 - 6x + 2'
+
 
 #add text
-plt.text(0.2, 0.7, r"$%s$" % expr, fontsize = 50)                            
+plt.text(0.1, 0.6, r"$%s$" % y, fontsize = 50)
+plt.text(0.1, 0.3, r"$%s$" % dy, fontsize = 50)
 
-#hide axes                                                           
-fig = plt.gca()                                  
-fig.axes.get_xaxis().set_visible(False)                                         
+#hide axes
+fig = plt.gca()
+fig.axes.get_xaxis().set_visible(False)
 fig.axes.get_yaxis().set_visible(False)
 
-plt.draw() #or savefig                                                          
+plt.draw() #or savefig
 plt.show()
--- graph.py
+++ graph.py
@@ -1,23 +1,23 @@
-import matplotlib.pyplot as plt                                                 
+import matplotlib.pyplot as plt
 import numpy as np
 
 ## Create functions and set domain length
-x = np.arange(0.0, 2.0, 0.01)
-y = x**2
-dy = 2*x - 1
+x = np.arange(-1, 3, 0.01)
 
-## Plot functions and a point where they intersect
-plt.plot(x, y)
-plt.plot(x, dy)
-plt.plot(1, 1, 'or')
+plt.plot(x, x**3 - 3*(x**2) + 2*x)
+plt.plot(0.39, 0.375, 'or')
+plt.plot(1.58, -0.4, 'og')
+plt.text(0.46, 0.375, '<- Max')
+plt.text(1.71, -0.42, '<- Min')
 
 ## Configure the graph
-plt.title('A Cool Graph')
+plt.title('y = x^3-3x^2 + 2x')
 plt.xlabel('X')
 plt.ylabel('Y')
-#plt.ylim([0, 4])
+plt.ylim([-2, 2])
 plt.grid(True)
-plt.legend(['y = x^2', 'y = 2x'], loc='upper left')
+#plt.legend(['y = x^2', 'y = 2x'], loc='upper left')
 
 ## Show the graph
+#plt.savefig()
 plt.show()
